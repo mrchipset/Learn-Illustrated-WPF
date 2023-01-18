38830309b713429406aed97d5d2f843b0191913a
X.F.Zhou <mrchip.zxf@outlook.com>
2023-01-18 15:57:08 +0800

    FEAT: create the first simple app

---
+++ Ch2/01_SimpleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace IllustratedWPF.HiThere
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Create the windwo object
+            Window myWindow = new Window();
+            myWindow.Title = "My Simple Window";
+            myWindow.Content = "Hi there!";
+
+            // Create Application
+            Application myApp = new Application();
+            myApp.Run(myWindow);
+        }
+    }
+}
