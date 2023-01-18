84e7ee2a96a9ca1a4ef0e2544a84a7f9be26dd96
X.F.Zhou <mrchip.zxf@outlook.com>
2023-01-18 17:03:02 +0800

    FEAT: add a App startup event handler

---
+++ Ch3/03_AppStartup/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+namespace IllustratedWPF.AppStartup
+{
+    class MyWindow : Window
+    {
+        public MyWindow()
+        {
+            Width = 300;
+            Height = 200;
+            Title = "My Program Window";
+            Content = "This application handles the Startup event.";
+        }
+    }
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            MyWindow window = new MyWindow();
+            Application application = new Application();
+            application.Startup += Application_Startup;
+            application.Run(window);
+        }
+
+        private static void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MessageBox.Show("The application is starting.", "Starting Message");
+        }
+    }
+}
