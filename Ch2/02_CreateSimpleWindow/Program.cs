3e660af16ee3df0ad8c5da447a0cdd584b7c83d1
X.F.Zhou <mrchip.zxf@outlook.com>
2023-01-18 16:12:07 +0800

    FEAT: add a window style demo

---
+++ Ch2/02_CreateSimpleWindow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace CreateSimpleWindowCode
+{
+    class MyWindow : Window
+    {
+        public MyWindow()
+        {
+            Width = 300;
+            Height = 200;
+            Title = "My Simple Window";
+            Content = "Hi There";
+        }
+    }
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            MyWindow window = new MyWindow();
+            window.Show();
+
+            Application application = new Application();
+            application.Run();
+        }
+    }
+}
---
+++ Ch2/03_WindowStyles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace _03_WindowStyles
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Window window = new Window();
+            window.Height = 75;
+            window.Width = 300;
+            window.Title = "WindowStyles";
+
+            window.WindowStyle = WindowStyle.SingleBorderWindow;
+            window.Content = window.WindowStyle.ToString();
+
+            Application app = new Application();
+            app.Run(window);
+        }
+    }
+}
